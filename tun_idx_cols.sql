ab6998eb328123e18f7276c9867834ee3cf6923f
dbasobrinho <roberto@dbasobrinho.com.br>
2024-12-13 22:30:51 -0300

    new

--- tun_idx_cols.sql
+++ tun_idx_cols.sql
@@ -14,21 +14,52 @@
 set verify off tab off
 set pages 100 lines 200 
 
-col index_owner for a16 heading 'Owner'
-col index_name  for a40 heading 'Indice'
-col column_name for a20 heading 'Coluna'
-col column_position for 999 heading 'Posicao'
+COLUMN index_owner FORMAT A15
+COLUMN index_name FORMAT A30
+COLUMN column_name FORMAT A20
+COLUMN column_position FORMAT 999
+COLUMN num_distinct FORMAT 9999999
+COLUMN clustering_factor FORMAT 9999999
 
 accept name prompt 'Nome da tabela: '
 prompt
 
 BREAK ON index_owner SKIP 1 ON index_name SKIP 1 
 
-select index_owner, index_name, column_name, column_position
-from dba_ind_columns
-where table_name = upper('&name')
-order by index_owner, index_name, column_position
-/
+
+SELECT 
+    ic.index_owner,
+    ic.index_name,
+    ic.column_name,
+    ic.column_position,
+    ts.num_distinct,
+    idx.clustering_factor,
+    t.num_rows
+FROM 
+    dba_ind_columns ic
+LEFT JOIN 
+    dba_tab_col_statistics ts
+ON 
+    ic.table_name = ts.table_name
+    AND ic.column_name = ts.column_name
+    AND ic.index_owner = ts.owner
+LEFT JOIN 
+    dba_indexes idx
+ON 
+    ic.index_owner = idx.owner
+    AND ic.index_name = idx.index_name
+LEFT JOIN 
+    dba_tables t
+ON 
+    ic.index_owner = t.owner
+    AND ic.table_name = t.table_name
+WHERE 
+    ic.table_name = UPPER('&name')
+ORDER BY 
+    ic.index_owner, 
+    ic.index_name, 
+    ic.column_position;
+
 
 --
 -- Fim
